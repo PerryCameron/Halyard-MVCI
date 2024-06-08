df7250ca4771e91abfc132f1bd9d10f9bc6a1947
PerryCameron <pcameron28@yahoo.com>
2024-06-08 13:35:38 -0400

    Added version to log

--- src/main/java/org/ecsail/BaseApplication.java
+++ src/main/java/org/ecsail/BaseApplication.java
@@ -7,18 +7,39 @@ import org.ecsail.mvci_main.MainController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 
 public class BaseApplication extends Application {
 
     public static Stage primaryStage;
     private static final Logger logger = LoggerFactory.getLogger(BaseApplication.class);
+
     public static void main(String[] args) {
         launch(args);
     }
 
+    private static String logAppVersion() {
+        Properties properties = new Properties();
+        try (InputStream input = BaseApplication.class.getClassLoader().getResourceAsStream("app.properties")) {
+            if (input == null) {
+                logger.error("Sorry, unable to find app.properties");
+                return "unknown" ;
+            }
+            properties.load(input);
+            String appVersion = properties.getProperty("app.version");
+            logger.info("Halyard Application version: " + appVersion);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return properties.getProperty("app.version");
+    }
+
     @Override
     public void start(Stage stage) {
-        logger.info("Starting application");
+        logAppVersion();
         primaryStage = stage;
         primaryStage.setScene(new Scene(new MainController().getView()));
         primaryStage.getScene().getStylesheets().addAll(
@@ -30,6 +51,5 @@ public class BaseApplication extends Application {
                 "css/dark/table_changes.css",
                 "css/dark/invoice.css");
         primaryStage.show();
-        logger.info("Primary stage shown");
     }
 }
